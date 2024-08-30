ff06e01e0de0c0057060910c4b7b6d3bf5ad514e
razia.sultana <razia.sultana@ipdcbd.com>
2024-08-31 00:01:53 +0600

    Add all my files

---
+++ src/main/java/com/tdd/eCommerceApp/ECommerceApp.java
@@ -0,0 +1,16 @@
+package com.tdd.eCommerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ECommerceApp.class, args);
+		System.out.println("Application is running successfully");
+	}
+
+
+}
